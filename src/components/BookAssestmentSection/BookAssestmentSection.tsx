210880735af1dd195b44711cad069b735cf40313
Ivan Santamans <ivansantamans@gmail.com>
2025-03-12 21:39:10 -0300

    book an assestment button redirecting to whatsapp

--- src/components/BookAssestmentSection/BookAssestmentSection.tsx
+++ src/components/BookAssestmentSection/BookAssestmentSection.tsx
@@ -8,7 +8,11 @@ function BookAssestmentSection() {
             <h2>RECUPERA TU PAZ EN EL TRABAJO</h2>
             <h1>Toma el control y pon fin al <span className="mobile-hide"><br /></span> acoso laboral de una vez <span className="mobile-hide"><br /></span> por todas</h1>
             <p>Sentirte atrapada en un ambiente tóxico no es <span className="mobile-hide"><br /></span> tu única opción. Con SafeWork, accede a un <span className="mobile-hide"><br /></span> plan integral que combina asesoría legal, <span className="mobile-hide"><br /></span> apoyo psicológico y estratégias efectivas para <span className="mobile-hide"><br /></span> enfrentar el acoso laboral con confianza.</p>
-            <button>Agenda una asesoría SafeWork</button>
+            <button 
+              onClick={() => window.open("https://wa.me/34622377041?text=Hola,%20me%20gustaría%20agendar%20una%20asesoría%20SafeWork.", "_blank")}
+            >
+              Agenda una asesoria SafeWork
+          </button>
         </TextContainer>
         <ImageContainer>
             <img src="/images/profesional.jpg" alt="" />
