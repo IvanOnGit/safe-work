73a8c452c2247f81c28da81f03cf4d084731d83f
Ivan Santamans <ivansantamans@gmail.com>
2025-04-29 11:13:05 -0300

    mobile responsiveness added

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -18,6 +18,29 @@ export const Container = styled.section`
 
     iframe {
         border-radius: 1rem;
+
+        @media (max-width: 768px) {
+            width: 22rem;
+            height: 22rem;
+        }
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        height: auto;
+        padding: 2rem;
+
+        gap: 2rem;
+
+        h3 {
+            width: auto;
+        }
+
+        h3:first-of-type {
+            width: 20rem;
+            text-align: justify;
+            font-size: 0.9rem;
+        }
     }
 `;
 
@@ -50,7 +73,25 @@ export const InfoContainer = styled.div`
       width: 1.2rem;
       margin-bottom: 0.2rem;
     }
+
+    @media (max-width: 768px) {
+      width: 22rem;
+      font-size: 0.9rem;
+    }
   }
+
+  @media (max-width: 768px) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  h3 {
+    width: 100%;
+    text-align: center;
+    margin-left: 12rem;
+  }
+}
 `;
 
 export const StyledLink = styled(Link)`
