083567161f4e373b593f8561cb378b7d19988ec0
Ivan Santamans <ivansantamans@gmail.com>
2025-05-02 09:21:46 -0300

    back changes

--- src/components/WhyUs/styles.ts
+++ src/components/WhyUs/styles.ts
@@ -1,48 +1,47 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  height: auto;
-  min-height: 40rem;
-  background-color: #E6E6E6;
+  height: 40rem;
+  background-color: 
+#E6E6E6;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
   margin-top: 1rem;
-  padding: 2rem 0;
 
   @media (max-width: 768px) {
-    background-color: #E6E6E6;
+    background-color: 
+#E6E6E6;
     top: 0;
     margin-top: 3rem;
-    padding: 2rem 0 3rem 0;
+    padding-top: 0;
+    height: auto;
   }
 `;
 
 export const TitleContainer = styled.div`
-  width: 100%;
-  max-width: 72rem;
   margin-bottom: 1rem;
-  padding: 0 1rem;
+  margin-right: 36.5rem;
   text-align: left;
 
   h1 {
     font-size: 1.5rem;
     font-family: 'Inter', sans-serif;
-    color: #001C3C;
+    color: 
+#001C3C;
   }
 
   @media (max-width: 768px) {
-    padding: 0 2rem;
+    margin-right: 1rem;
+    margin-left: 2rem;
   }
 `;
 
 export const WhyUsContainer = styled.div`
   display: flex;
   gap: 2rem;
-  width: 100%;
-  max-width: 72rem;
-  padding: 0 1rem;
+  margin-bottom: 2rem;
 
   img {
     height: 31rem;
@@ -56,14 +55,14 @@ export const WhyUsContainer = styled.div`
       height: 20rem;
       width: 20rem;
       align-self: center;
-      margin-bottom: 2rem;
+      margin-bottom: 3rem;
       border-radius: 1rem;
     }
   }
 
   @media (max-width: 768px) {
     flex-direction: column-reverse;
-    padding: 0 2rem;
+    margin-bottom: 5rem;
   }
 `;
 
@@ -72,13 +71,12 @@ export const TextContainer = styled.div`
   flex-direction: column;
   gap: 1.65rem;
   width: 45rem;
-  max-width: 100%;
-  position: relative;
 
   p {
     font-family: 'Inter', sans-serif;
     font-weight: bold;
-    color: #001C3C;
+    color: 
+#001C3C;
     margin: 0;
     padding: 0;
     font-size: 1.1rem;
@@ -94,7 +92,8 @@ export const TextContainer = styled.div`
   }
 
   button {
-    background-color: #001C3C;
+    background-color: 
+#001C3C;
     color: white;
     border: none;
     border-radius: 0.5rem;
@@ -103,23 +102,26 @@ export const TextContainer = styled.div`
     cursor: pointer;
     width: 11rem;
     font-size: 0.9rem;
-    align-self: flex-start;
 
     @media (max-width: 768px) {
-      width: 100%;
+      position: absolute;
+      margin-top: 68rem;
+      width: 20rem;
       align-self: center;
       padding: 1rem;
       font-size: 1rem;
-      margin-top: 1rem;
     }
 
     &:hover {
-      background-color: #183a6c;
+      background-color: 
+#183a6c;
       transition: all 0.3s ease-in-out;
     }
   }
 
   @media (max-width: 768px) {
-    width: 100%;
+    margin-left: 2rem;
+    margin-right: 2rem;
+    width: 22rem;
   }
 `;
