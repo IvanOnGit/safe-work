39ca71525c8d81e35dd117bdefc477c123f42d0c
Ivan Santamans <ivansantamans@gmail.com>
2025-05-15 10:32:16 -0300

    404 error page added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import Privacy from './components/Privacy/Privacy';
 import Cookies from './components/Cookies/Cookies';
 import Legal from './components/Legal/Legal';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/privacidad" element={<Privacy />} />
         <Route path="/cookies" element={<Cookies />} />
         <Route path="/legal" element={<Legal />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,13 @@
+import { Container } from './styles';
+
+function ErrorPage() {
+    return (
+        <>
+            <Container>
+                <h1>404 Not Found Working</h1>
+            </Container>
+        </>
+  );
+}
+
+export default ErrorPage;
