10fc3b1830ca1e30a7529dfe5d51a06e41ec4926
Ivan Santamans <ivansantamans@gmail.com>
2025-10-21 20:09:09 -0300

    conversion event added to thank you page

--- src/components/ThankYou/ThankYou.tsx
+++ src/components/ThankYou/ThankYou.tsx
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 function ThankYou() {
+  useEffect(() => {
+    // Disparar el evento de conversión al cargar la página
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      'event': 'cal_booking_completed'
+    });
+  }, []); // Array vacío = se ejecuta solo una vez al montar el componente
 
   return (
     <Container>
-        <h1>¡Gracias!</h1>
+      <h1>¡Gracias!</h1>
+      <p>Hemos recibido tu solicitud correctamente.</p>
+      <p>Nos pondremos en contacto contigo lo antes posible.</p>
     </Container>
   );
 }
