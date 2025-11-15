ec580aded1c4865e4d348562ff17d8af3b25e8d7
Ivan Santamans <ivansantamans@gmail.com>
2025-11-15 12:58:25 -0300

    new hashing

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // ← CAMBIAR A ESTO
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`  // ← FORZAR NUEVO HASH
-      }
-    }
-  }
+  base: '/'
 })
